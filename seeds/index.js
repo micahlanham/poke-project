e3b52b4412f9b0ce40d56768f5b7b5889efd844f
Lightfooted <vesselofbalance@hotmail.com>
2021-09-18 15:20:14 -0400

    back end structure. needs a lot of work

--- seeds/index.js
+++ seeds/index.js
@@ -1,7 +1,7 @@
 const seedCategories = require('./category-seeds');
-const seedProducts = require('./product-seeds');
-const seedTags = require('./tag-seeds');
-const seedProductTags = require('./product-tag-seeds');
+const seedpocketmonsters = require('./pokemon-seeds');
+const seedpokemontypes = require('./type-seeds');
+const seedusers = require('./user-seeds');
 
 const sequelize = require('../config/connection');
 
