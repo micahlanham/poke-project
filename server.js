9f5ac6368559faf012d1046aac0c99775ebb07a5
Lightfooted <vesselofbalance@hotmail.com>
2021-09-20 21:51:23 -0400

    added handlebars and more middleware. work in progress

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const express = require('express');
-const routes = require('./routes');
 const sequelize = require('./config/connection');
-
 const app = express();
 const PORT = process.env.PORT || 3001;
+const exphbs = require('express-handlebars');
 
+//Middle-ware
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Handlebar
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+//Routes
+const routes = require('./routes');
 
 app.use(routes);
 
