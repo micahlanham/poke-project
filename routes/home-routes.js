31069af666c1b89358a057da0da473ac5e497656
micahlanham <84043453+micahlanham@users.noreply.github.com>
2021-09-26 00:05:18 -0400

    removed login

--- routes/home-routes.js
+++ routes/home-routes.js
@@ -1,17 +1,10 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
-const { User, Pokemon, } = require('../models');
+const { Pokemon, } = require('../models');
 
 // to do display all pokemon for homepage
 
 
-router.get('/login', (req, res) => {
-  if (req.session.loggedIn) {
-    res.redirect('/');
-    return;
-  }
 
-  res.render('login');
-});
 
 module.exports = router;
